229d3cdffb7464fe764cc9a66c1dd8ff0cd8a900
Muhammad Ilfanza <83349619+kaspersky21@users.noreply.github.com>
2022-12-16 22:33:47 +0700

    Add files via upload

---
+++ UAS/nomor4.py
@@ -0,0 +1,9 @@
+N = 1000
+delta = 1
+for n in range (1, N+1):
+    delta = n**2
+    print(n,"pangkat 2 =", delta)
+
+print(" ")
+print("KESIMPULAN")
+print("Jumlah dari 1000 kuadrat bilangan pertama adalah: ", N**2)
